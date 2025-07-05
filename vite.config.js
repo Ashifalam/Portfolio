533b8dfd422cb9b399ded0f58a48706cdcb07a09
Aquif Zubair <az@aquif-3.local>
2025-07-05 19:48:38 +0530

    Remove all tests and test configurations

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.jsx',
-    css: true,
-    environmentOptions: {
-      jsdom: {
-        resources: 'usable',
-      },
-    },
-  },
   plugins: [react()],
   build: {
     // Enable code splitting
